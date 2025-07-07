f451eb1b49ec0898600d994e140bf95160b4cf1b
Benjamin Walkenhorst <krylon@posteo.de>
2025-07-07 18:17:01 +0200

    scanner - add active flag

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -2,18 +2,20 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 03. 07. 2025 by Benjamin Walkenhorst
 // (c) 2025 Benjamin Walkenhorst
-// Time-stamp: <2025-07-03 21:54:56 krylon>
+// Time-stamp: <2025-07-07 16:27:00 krylon>
 
 package scanner
 
 import (
 	"log"
+	"sync/atomic"
 
 	"github.com/blicero/carebear/scanner/command"
 )
 
 // Scanner traverses IP networks looking for Devices.
 type Scanner struct {
-	log  *log.Logger
-	cmdQ chan command.Command
+	log        *log.Logger
+	cmdQ       chan command.Command
+	activeFlag atomic.Bool
 }
