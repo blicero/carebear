e80b7a7a88a3bc94d2cba17ee86ab9205063eb61
Benjamin Walkenhorst <krylon@posteo.de>
2025-07-08 11:57:48 +0200

    start implementing scanner

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -2,14 +2,18 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 03. 07. 2025 by Benjamin Walkenhorst
 // (c) 2025 Benjamin Walkenhorst
-// Time-stamp: <2025-07-07 16:27:00 krylon>
+// Time-stamp: <2025-07-08 11:57:28 krylon>
 
 package scanner
 
 import (
+	"fmt"
 	"log"
 	"sync/atomic"
 
+	"github.com/blicero/carebear/common"
+	"github.com/blicero/carebear/database"
+	"github.com/blicero/carebear/logdomain"
 	"github.com/blicero/carebear/scanner/command"
 )
 
@@ -18,4 +22,25 @@ type Scanner struct {
 	log        *log.Logger
 	cmdQ       chan command.Command
 	activeFlag atomic.Bool
+	db         *database.Database
 }
+
+// New creates a new Scanner.
+func New() (*Scanner, error) {
+	var (
+		err error
+		s   = &Scanner{
+			cmdQ: make(chan command.Command),
+		}
+	)
+
+	if s.log, err = common.GetLogger(logdomain.Scanner); err != nil {
+		var ex = fmt.Errorf("Cannot create Logger for Scanner: %w", err)
+		return nil, ex
+	} else if s.db, err = database.Open(common.DbPath); err != nil {
+		s.log.Printf("[ERROR] Cannot open database: %s\n", err.Error())
+		return nil, err
+	}
+
+	return s, nil
+} // func New() (*Scanner, error)
